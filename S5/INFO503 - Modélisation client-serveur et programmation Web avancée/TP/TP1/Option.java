d6fda60aeb3e20014e9ec74c9a0b28dff86b9c4e
Verminkin <Verminkin@users.noreply.github.com>
2019-09-15 15:33:35 +0200

    Create Option.java
    Options up

---
+++ S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/TP1/Option.java
@@ -0,0 +1,27 @@
+Class Options{
+  //attributs
+  protected boolean GPS;
+  protected boolean Vitre;
+  protected boolean Clim;
+  //Constructeur
+  Options(){
+    GPS=false;
+    Vitre=false;
+    clim=false;
+  }
+  Options(Boolean GPS,boolean Vitre,boolean Clim){
+    this.GPS=GPS;
+    this.Vitre=Vitre;
+    this.clim=clim;
+  }
+  Options(Options op){
+    this.GPS=op.GPS;
+    this.Vitre=op.Vitre;
+    this.clim=op.clim;
+  }
+  //getter
+  public boolean getGPS(){
+    return GPS;
+  }
+  public boolean
+}
