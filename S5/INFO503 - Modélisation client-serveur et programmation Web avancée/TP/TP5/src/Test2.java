ca278a27aa61654e6eb5f95b99d1b4ffa6252620
Thomas Gigout <thomasgpro@hotmail.com>
2019-10-17 23:35:41 +0200

    up

--- S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/TP5/src/Test2.java
+++ S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/TP5/src/Test2.java
@@ -1,8 +1,76 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 import mediatheque2.*;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONString;
+
+
 class Test2{
 	public static void main(String [] args){
-	Personne a = new Personne("erf","1/1/00");
+		if(args.length!=1){
+			System.err.println("Erreur : vous devez spécifier le nom du fichier JSON.\n\nUsage : java LecteurJSON fichier.json\n\toù 'fichier.json' est le nom du fichier à ouvrir");
+			System.exit(-1);
+		}
+
+		//Ouverture du fichier
+		FileInputStream fs=null;
+		try{
+			fs = new FileInputStream(args[0]);
+		}
+		catch(FileNotFoundException e){
+			System.err.println("Fichier"+ args[0] + " introuvable !");
+			System.exit(-1);
+		}
+		// Récupération de la chaîne JSON depuis le fichier
+		String json = new String();
+		Scanner s = new Scanner(fs);
+		while(s.hasNext()){
+			json+=s.nextLine();
+		}
+		s.close();
+		json = json.replaceAll("[\t ]","");
+
+		//Fermeture du fichier
+		try{
+			fs.close();
+		}
+		catch(IOException e){
+			System.err.println("Erreur lors de la fermeture du fichier !");
+			System.err.println(e);
+			System.exit(-1);
+		}
+
+		//Création d'un objet JSON
+		JSONObject objet = new JSONObject(json);
+		System.out.println("Contenu JSON : ");
+		System.out.println(json);
+
+		//Affichage à l'écran
+		JSONArray tableau = objet.getJSONArray("contacts");
+		System.out.println("Liste des personnes : ");
+		for(int i=0 ; i<tableau.length() ; i++){
+			JSONObject element = tableau.getJSONObject(i);
+			System.out.print("nom="+element.getString("nom"));
+			System.out.print(", prenom="+element.getString("prenom"));
+			System.out.print(", age="+element.getInt("age"));
+		}
+	}
+}
+
+
+
+// javac -d .\classes\ -sourcepath .\src\ -cp .\lib\json-20190722.jar .\src\Test.java
+// java -cp ".\classes\;.\lib\json-20190722.jar" Test
+
+
+
+/* TEST DE VERIFICATION DE SERIALISATION
+
+Personne a = new Personne("erf","1/1/00");
 	Document d =new Livre("cheh",a);
 	Document f=new Film("kek",a,12.5);
 	Mediatheque m=new Mediatheque();
@@ -26,7 +94,4 @@ class Test2{
 
 	JSONObject tab= new JSONObject(T);
 	System.out.println(tab);
-	}
-}
-// javac -d .\classes\ -sourcepath .\src\ -cp .\lib\json-20190722.jar .\src\Test.java
-// java -cp ".\classes\;.\lib\json-20190722.jar" Test
+*/
---
+++ S5/INFO504 - Introduction au génie logiciel/TP4/test_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+int
+main(int argc, char** argv)
+{
+  int value;
+  // value = 1;
+
+  std::cout << value << std::endl;
+  
+  return EXIT_SUCCESS;
+}
