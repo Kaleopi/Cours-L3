e4ec8d70b571d7e8a46cda6d31456f65487ff464
Thomas Gigout <thomasgpro@hotmail.com>
2019-10-22 13:49:34 +0200

    up

---
+++ S5/INFO504 - Introduction au génie logiciel/TP5/info501_tp1/TP1/prog_liste.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graphe.h"
+#include "cellule.h"
+#include "liste.h"
+
+int main(int argc, char *argv[]){
+  Graphe *g = malloc(sizeof(Graphe));
+  initialiser_graphe(g,argv[1]);
+  afficher_graphe(g);
+  detruire_graphe(g);
+  afficher_graphe(g);
+  return 0;
+}
+
+//MAIN de test pour les listes et cellules
+/*
+int main(){
+  Liste l;
+  Liste l2;
+  initListe(&l);
+  initListe(&l2);
+  Cellule *c = malloc(sizeof(Cellule));
+  initCellule(c,5);
+  insererListe(&l,c);
+  Cellule *c2 = malloc(sizeof(Cellule));
+  initCellule(c2,7);
+  insererListe(&l,c2);
+  afficherListe(&l);
+  supprimerCellule(&l,c);
+  afficherListe(&l);
+  afficherListe(&l2);
+  supprimerListe(&l2);
+  afficherListe(&l2);
+
+  return 0;
+}*/
