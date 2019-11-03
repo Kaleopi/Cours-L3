6d26eac5551cd34e173663141b77217a954d94c7
Verminkin <Verminkin@users.noreply.github.com>
2019-11-03 18:07:46 +0100

    fdgdf
    fddf

---
+++ S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/projet_sondages/ServeurSondage/SondageIndex.php
@@ -0,0 +1,31 @@
+<?php
+spl_autoload_register(function ($class) {
+    include 'classes/' . $class . '.class.php';
+});
+header("Content-type: application/json; charset=utf-8");
+
+// Préparation de la requête
+$options = [
+  'http' =>
+  [
+    'method'  => 'POST',
+    'header'  => 'Content-type: application/x-www-form-urlencoded',
+    'content' => $_POST
+    ]
+  ];
+
+  if($_POST['donnees']=="createSondage"){
+    // var_dump($_POST);
+     $_POST['questions'] = json_decode($_POST['questions']);
+     $_POST['choix'] = json_decode($_POST['choix']);
+
+    $sondage = new Sondage($_POST['responsable'],$_POST['titre'],$_POST['questions'],$_POST['choix']);
+    // var_dump($sondage->getQuestions());
+    // var_dump( $sondage);
+    $monfichier = fopen('sondage.txt', 'r+');
+    // 2 : on écrit
+    fputs($monfichier, $_POST['responsable'].$_POST['titre'].$_POST['questions'].$_POST['choix']);
+    // 3 : quand on a fini de l'utiliser, on ferme le fichier
+    fclose($monfichier);
+   }
+?>
