ac6f3bd8d2a386a1bad338ec4559db5837db6f41
Thomas <thomasgpro@hotmail.com>
2019-11-03 18:18:43 +0100

    up

--- S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/projet_sondages/ServeurSondage/SondageIndex.php
+++ S5/INFO503 - Modélisation client-serveur et programmation Web avancée/TP/projet_sondages/ServeurSondage/SondageIndex.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function ($class) {
     include 'classes/' . $class . '.class.php';
 });
-header("Content-type: application/json; charset=utf-8");
+// header("Content-type: application/json; charset=utf-8");
 
 // Préparation de la requête
 $options = [
@@ -22,6 +22,12 @@ $options = [
     $sondage = new Sondage($_POST['responsable'],$_POST['titre'],$_POST['questions'],$_POST['choix']);
     // var_dump($sondage->getQuestions());
     // var_dump( $sondage);
+    $sondage = json_encode($sondage);
+    $monfichier = fopen('../src/sondage.json', 'w+');
+    // 2 : on écrit
+    fputs($monfichier, $sondage);
+    // 3 : quand on a fini de l'utiliser, on ferme le fichier
+    fclose($monfichier);
     
    }
 ?>
