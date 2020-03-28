47adae04f2139e67ce191fd0f05345d0fe9e2fe4
Verminkin <nurglitsh@linux.home>
2020-03-28 15:47:22 +0100

    kek

--- S6/INFO601/projet3_mix/message.h
+++ S6/INFO601/projet3_mix/message.h
@@ -4,6 +4,13 @@
 #include <pthread.h>
 #define TYPE_CONNEXION 0
 #define TYPE_CONNEXION_AUTHORIZED 0
+
+typedef struct case_tag {					/* Description d'une case sur la grille de simulation */
+	int element;							/* Ce qui est present sur la case */
+	pthread_t *poisson;						/* Identifiant du thread de la poisson presente sur la case */
+	pthread_mutex_t mutex;					/* Protection de la case */
+} case_t;
+
 /**
  * @struct requete_t
  * @field type type de la requête (connexion, etc)
@@ -19,13 +26,9 @@ typedef struct{
 typedef struct{
   long type;
   int port;
+  case_t grille;
 }reponse_t;
 
-typedef struct case_tag {					/* Description d'une case sur la grille de simulation */
-	int element;							/* Ce qui est present sur la case */
-	pthread_t *poisson;						/* Identifiant du thread de la poisson presente sur la case */
-	pthread_mutex_t mutex;					/* Protection de la case */
-} case_t;
 
 typedef struct coord_tag {					/* Coordonnees d'une case sur la grille de simulation*/
 	int y;
