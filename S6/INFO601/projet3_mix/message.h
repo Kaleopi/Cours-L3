d9fb271bb0b73cab0aef00efc0ae7a4a5e806bbf
Verminkin <Verminkin@github.com>
2020-04-10 12:58:57 +0200

    k

--- S6/INFO601/projet3_mix/message.h
+++ S6/INFO601/projet3_mix/message.h
@@ -7,31 +7,31 @@
 
 #define TYPE_CONNEXION 0
 #define TYPE_CONNEXION_AUTHORIZED 0
-
+/*structure thread poisson*/
 typedef struct{					/* Description d'une case sur la grille de simulation */
 	int element;							/* Ce qui est present sur la case */
 	pthread_t *poisson;						/* Identifiant du thread de la poisson presente sur la case */
 	pthread_mutex_t mutex;					/* Protection de la case */
 }case_t;
-
+/*structure du joueur*/
 typedef struct{
 	int id;
 	int furtif;
 	int points;
 	int poireaus;
 }joueur_t;
-
+/*structure test d'update message serveur*/
 typedef struct{
 	int what;
 	int posx;
 	int posy;
 	int type;
 }update_t;
-
+/*grille étazng*/
 typedef struct{
 	int grille[NB_LIGNES_SIM][NB_COL_SIM];
 }grille_t;
-
+/*structure des poissons*/
 typedef struct{
 	int id;
 	int ETAT;
@@ -59,7 +59,7 @@ typedef struct{
   int port;
 }reponse_t;
 
-
+/*structure de stockage des positions et des infos poissons à passer au pthread create */
 typedef struct{					/* Coordonnees d'une case sur la grille de simulation*/
 	int y;
 	int x;
