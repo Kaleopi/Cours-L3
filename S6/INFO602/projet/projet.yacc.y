31ed5b0760f0c2cfc142b2b1820a5dfc2b5ff954
Kaleopi <thomasgpro@hotmail.com>
2020-04-11 04:57:47 +0200

    upu

--- S6/INFO602/projet/projet.yacc.y
+++ S6/INFO602/projet/projet.yacc.y
@@ -1,53 +1,123 @@
 %{
 #include <stdio.h>
 #include <stdlib.h>
+extern FILE *yyin;
 
 int yylex();
 void yyerror(const char *erreurMsg);
 %}
+%define parse.error verbose
 
 %union {
-  char* string;
+  int intval;
+  char string[100];
 }
-%token <string> TESTJSON
-%token <string> ENTIER
+
+%token <intval> ENTIER
 %token <string> NOM
 %token VRAI FAUX
 %token HAUTEUR LARGEUR X Y TYPEJSON BLOC BILLE CAISSE TROU CASE CASES DEBUT DIRECTION BAS HAUT DROITE GAUCHE
-%token TYPE PROC FUNC F_AVANCE F_DROITE F_GAUCHE FINTQ FINPROC FINFUNC INFEG SUPEG EGEG INF SUP
 
+%type <string> json
+%type <string> lelems
+%type <string> elemjson
+%type <string> debutjson
+%type <string> direction
+%type <string> tabcases
+%type <string> scase
+%type <string> cases
+%type <string> type
 %%
-json: TESTJSON{
-  printf("%s",$1);
-}
-/* json: '{' elemjson ',' elemjson ',' elemjson ',' elemjson '}'
-    {
+json:
+    '{' lelems '}'{
+      printf("%s",$2);
+    };
 
-    } */
-json: '{'elemjson ',' json | elemjson ',' json | '}';
+lelems:
+    elemjson ',' lelems{
+      printf("%s",$1);
+    }
+    |
+    elemjson{
+      printf("%s",$1);
+    };
 
-'{'elemjson ',' elemjson ',' '}'
+elemjson:
+    LARGEUR ':' ENTIER {
+      printf("Largeur plateau : %d\n",$3);
+    }
+    |
+    HAUTEUR ':' ENTIER {
+      printf("Hauteur plateau : %d\n",$3);
+    }
+    |
+    DEBUT ':' debutjson {}
+    |
+    CASES ':' tabcases {};
 
+debutjson:
+    '{'X':'ENTIER','Y':'ENTIER','DIRECTION':'direction'}'{
+      printf("Robot x : %d y : %d direction : %s\n",$4,$8,$12);
+    };
 
+direction:
+    BAS {sprintf($$,"BAS");}
+    |
+    HAUT {sprintf($$,"HAUT");}
+    |
+    DROITE {sprintf($$,"DROITE");}
+    |
+    GAUCHE {sprintf($$,"GAUCHE");};
+
+tabcases:
+    '['cases']' {};
+
+cases:
+    scase ',' cases
+    |
+    scase {};
+
+scase:
+    '{'X':'ENTIER','Y':'ENTIER','TYPEJSON':'type'}'{
+      printf("Case x : %d y : %d type : %s\n",$4,$8,$12);
+    };
+
+type:
+    CAISSE {
+      sprintf($$,"CAISSE");
+    }
+    |
+    TROU {
+      sprintf($$,"TROU");
+    }
+    |
+    BILLE {
+      sprintf($$,"BILLE");
+    }
+    |
+    BLOC {
+      sprintf($$,"BLOC");
+    };
 %%
 
 int main(int argc, char* argv[]) {
-  /*changer les entrées pour lire le fichier*/
-  if((yyin=fopen(argv[1],"r"))==NULL){
+  FILE* fd;
+  if((fd=fopen(argv[1],"r"))==NULL){
     fprintf(stderr, "Erreur lors de l'ouverture du fichier \"%s\"",argv[1]);
     exit(EXIT_FAILURE);
   }
+  yyin = fd;
   yyparse();
-  if((fclose(yyin))==EOF){
+  if((fclose(fd))==EOF){
     fprintf(stderr, "Erreur lors de la fermeture du fichier");
   };
-  if((yyin=fopen(argv[2],"r"))==NULL){
+  /* if((yyin=fopen(argv[2],"r"))==NULL){
     printf("Erreur lors de l'ouverture du fichier \"%s\"",argv[2]);
     exit(EXIT_FAILURE);
   }
   yyparse();
   if((fclose(yyin))==EOF){
-    fprintf(stderr, "Erreur lors de la fermeture du fichier");
-  };
+    printff(stderr, "Erreur lors de la fermeture du fichier");
+  }; */
   return EXIT_SUCCESS;
 }
---
+++ S6/INFO602/projet/y.tab.h
@@ -0,0 +1,117 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    ENTIER = 258,
+    NOM = 259,
+    VRAI = 260,
+    FAUX = 261,
+    HAUTEUR = 262,
+    LARGEUR = 263,
+    X = 264,
+    Y = 265,
+    TYPEJSON = 266,
+    BLOC = 267,
+    BILLE = 268,
+    CAISSE = 269,
+    TROU = 270,
+    CASE = 271,
+    CASES = 272,
+    DEBUT = 273,
+    DIRECTION = 274,
+    BAS = 275,
+    HAUT = 276,
+    DROITE = 277,
+    GAUCHE = 278
+  };
+#endif
+/* Tokens.  */
+#define ENTIER 258
+#define NOM 259
+#define VRAI 260
+#define FAUX 261
+#define HAUTEUR 262
+#define LARGEUR 263
+#define X 264
+#define Y 265
+#define TYPEJSON 266
+#define BLOC 267
+#define BILLE 268
+#define CAISSE 269
+#define TROU 270
+#define CASE 271
+#define CASES 272
+#define DEBUT 273
+#define DIRECTION 274
+#define BAS 275
+#define HAUT 276
+#define DROITE 277
+#define GAUCHE 278
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 11 "projet.yacc.y" /* yacc.c:1909  */
+
+  int intval;
+  char string[100];
+
+#line 105 "y.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
